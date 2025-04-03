f7f14b47ac721d28112d683888da9924cb942513
Lucas Garcia Arredondo <lucas.g.a@icloud.com>
2025-04-03 08:49:35 -0600

    Update Limpieza.sql

--- Limpieza.sql
+++ Limpieza.sql
@@ -3,28 +3,28 @@ create table partido_backup as table partido;
 update partido
 set
   season = lower(trim(replace(replace(season, '.', ''), ',', ''))),
-  home_team = lower(trim(replace(replace(home_team, '.', ''), ',', ''))),
-  away_team = lower(trim(replace(replace(away_team, '.', ''), ',', ''))),
+  home_team = trim(replace(replace(home_team, '.', ''), ',', '')),
+  away_team = trim(replace(replace(away_team, '.', ''), ',', '')),
   full_time_result = lower(trim(replace(replace(full_time_result, '.', ''), ',', ''))),
   half_time_result = lower(trim(replace(replace(half_time_result, '.', ''), ',', ''))),
   referee = lower(trim(replace(replace(referee, '.', ''), ',', ''))),
   league = lower(trim(replace(replace(league, '.', ''), ',', '')));
 
 -- manchester united
-update partido set home_team = 'manchester united' where home_team in ('man utd', 'manchester utd');
-update partido set away_team = 'manchester united' where away_team in ('man utd', 'manchester utd');
+update partido set home_team = 'Manchester United' where home_team in ('Man Utd', 'manchester utd');
+update partido set away_team = 'Manchester United' where away_team in ('Man Utd', 'manchester utd');
 
 -- manchester city
-update partido set home_team = 'manchester city' where home_team in ('man city', 'manchester c');
-update partido set away_team = 'manchester city' where away_team in ('man city', 'manchester c');
+update partido set home_team = 'Manchester City' where home_team in ('Man City', 'manchester c');
+update partido set away_team = 'Manchester City' where away_team in ('Man City', 'manchester c');
 
--- tottenham hotspur
-update partido set home_team = 'tottenham hotspur' where home_team = 'tottenham';
-update partido set away_team = 'tottenham hotspur' where away_team = 'tottenham';
+-- ipswich
+update partido set home_team = 'Ipswich' where home_team = 'Ipswich Town';
+update partido set away_team = 'Ipswich' where home_team = 'Ipswich Town';
 
--- wolverhampton wanderers
-update partido set home_team = 'wolverhampton wanderers' where home_team = 'wolves';
-update partido set away_team = 'wolverhampton wanderers' where away_team = 'wolves';
+-- brighton
+update partido set home_team = 'Brighton' where home_team = 'Brighton & Hove Albion';
+update partido set away_team = 'Brighton' where home_team = 'Brighton & Hove Albion';
 
 -- árbitros
 update partido set referee = 'martin dean' where referee = 'm dean';
